02f4f5462fd6b9b66c5054deb89b0b05070b072a
Dan Lionis <26198474+danlionis@users.noreply.github.com>
2024-05-14 17:58:13 +0200

    initial project commit

--- ebpf-labs/project/ebpf/l4_lb.bpf.c
+++ ebpf-labs/project/ebpf/l4_lb.bpf.c
@@ -13,9 +13,164 @@
 #include <stddef.h>
 #include <stdint.h>
 
+/* This is the data record stored in the map */
+struct backend {
+    __be32 ip;
+    __u64 num_flows;
+    __u64 num_packets;
+};
+
+struct {
+    __uint(type, BPF_MAP_TYPE_ARRAY);
+    __type(key, int);
+    __type(value, struct backend);
+    __uint(max_entries, 1024);
+} backend_map SEC(".maps");
+
+struct connection {
+    __be32 dst_addr;
+    __be32 src_addr;
+    __be16 dst_port;
+    __be16 src_port;
+};
+
+struct {
+    __uint(type, BPF_MAP_TYPE_HASH);
+    __type(key, struct connection);
+    __type(value, struct in_addr);
+    __uint(max_entries, 1024);
+} connections_map SEC(".maps");
+
+static __always_inline int parse_ethhdr(void *data, void *data_end, __u16 *nh_off,
+                                        struct ethhdr **ethhdr) {
+    struct ethhdr *eth = (struct ethhdr *)data;
+    int hdr_size = sizeof(*eth);
+
+    /* Byte-count bounds check; check if current pointer + size of header
+     * is after data_end.
+     */
+    if ((void *)eth + hdr_size > data_end)
+        return -1;
+
+    *nh_off += hdr_size;
+    *ethhdr = eth;
+
+    return eth->h_proto; /* network-byte-order */
+}
+
+static __always_inline int parse_iphdr(void *data, void *data_end, __u16 *nh_off,
+                                       struct iphdr **iphdr) {
+    struct iphdr *ip = data + *nh_off;
+    int hdr_size;
+
+    if ((void *)ip + sizeof(*ip) > data_end)
+        return -1;
+
+    hdr_size = ip->ihl * 4;
+
+    /* Sanity check packet field is valid */
+    if (hdr_size < sizeof(*ip))
+        return -1;
+
+    /* Variable-length IPv4 header, need to use byte-based arithmetic */
+    if ((void *)ip + hdr_size > data_end)
+        return -1;
+
+    // It can also be written as:
+    // if (data + *nh_off + hdr_size > data_end)
+    //    return -1;
+
+    *nh_off += hdr_size;
+    *iphdr = ip;
+
+    return ip->protocol;
+}
+
+static __always_inline int parse_udphdr(void *data, void *data_end, __u16 *nh_off,
+                                        struct udphdr **udphdr) {
+    struct udphdr *udp = data + *nh_off;
+    int hdr_size = sizeof(*udp);
+
+    if ((void *)udp + hdr_size > data_end)
+        return -1;
+
+    *nh_off += hdr_size;
+    *udphdr = udp;
+
+    int len = bpf_ntohs(udp->len) - sizeof(struct udphdr);
+    if (len < 0)
+        return -1;
+
+    return len;
+}
+
 SEC("xdp")
 int l4_lb(struct xdp_md *ctx) {
+    void *data_end = (void *)(long)ctx->data_end;
+    void *data = (void *)(long)ctx->data;
+
+    __u16 nf_off = 0;
+    struct ethhdr *eth;
+    int eth_type;
+
+    bpf_printk("Packet received");
+
+    eth_type = parse_ethhdr(data, data_end, &nf_off, &eth);
+
+    if (eth_type != bpf_ntohs(ETH_P_IP))
+        goto pass;
+
+    bpf_printk("Packet is IPv4");
+
+    // Handle IPv4 and parse ICMP
+    int ip_type;
+    struct iphdr *iphdr;
+    ip_type = parse_iphdr(data, data_end, &nf_off, &iphdr);
+
+    if (ip_type != IPPROTO_UDP)
+        goto pass;
+    bpf_printk("Packet is UDP");
+
+    struct udphdr *udphdr;
+    if (parse_udphdr(data, data_end, &nf_off, &udphdr) < 0)
+        goto drop;
+
+    __be32 dst_addr = iphdr->daddr;
+    __be32 src_addr = iphdr->saddr;
+    __be16 dst_port = udphdr->dest;
+    __be16 src_port = udphdr->source;
+
+    struct connection conn = {
+        .dst_addr = dst_addr,
+        .src_addr = src_addr,
+        .dst_port = dst_port,
+        .src_port = src_port,
+    };
+
+    struct backend *load;
+    struct in_addr *backend_ip = bpf_map_lookup_elem(&connections_map, &conn);
+
+    if (backend_ip == NULL) {
+        // conn not assigned to a backend
+    } else {
+        load = bpf_map_lookup_elem(&backend_map, &backend_ip->s_addr);
+    }
+
+    __sync_fetch_and_add(&load->num_packets, 1);
+
+    // encapsulate packet in new ip packet
+
+    // adjust head size
+    __u8 ihl = iphdr->ihl * 4;
+    if (bpf_xdp_adjust_head(ctx, ihl) != 0) {
+        bpf_printk("could not adjust head");
+        return XDP_DROP;
+    }
+
+drop:
     return XDP_DROP;
+pass:
+    return XDP_PASS;
 }
 
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
--- ebpf-labs/project/l4_lb.c
+++ ebpf-labs/project/l4_lb.c
@@ -7,6 +7,7 @@
 #include <fcntl.h>
 #include <linux/if_link.h>
 #include <netinet/in.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <sys/resource.h>
 #include <sys/socket.h>
@@ -15,6 +16,10 @@
 #include <argparse.h>
 #include <net/if.h>
 
+#include <cyaml/cyaml.h>
+
+#include "l4_lb.skel.h"
+
 #ifndef __USE_POSIX
 #define __USE_POSIX
 #endif
@@ -22,6 +27,96 @@
 
 #include "log.h"
 
+// Define the structure to hold the YAML data
+struct backend {
+    char *ip;
+};
+
+struct config {
+    char *vip;
+    struct backend *backends;
+    size_t backends_count;
+};
+
+static const cyaml_schema_field_t backend_field_schema[] = {
+    CYAML_FIELD_STRING_PTR("ip", CYAML_FLAG_POINTER, struct backend, ip, 0, CYAML_UNLIMITED),
+    CYAML_FIELD_END,
+};
+
+static const cyaml_schema_value_t backend_schema = {
+    CYAML_VALUE_MAPPING(CYAML_FLAG_DEFAULT, struct backend, backend_field_schema),
+};
+
+/* CYAML mapping schema fields array for the top level mapping. */
+static const cyaml_schema_field_t top_mapping_schema[] = {
+    CYAML_FIELD_STRING_PTR("vip", CYAML_FLAG_POINTER, struct config, vip, 0, CYAML_UNLIMITED),
+    CYAML_FIELD_SEQUENCE("backends", CYAML_FLAG_POINTER, struct config, backends, &backend_schema,
+                         0, CYAML_UNLIMITED),
+    CYAML_FIELD_END};
+
+/* CYAML value schema for the top level mapping. */
+static const cyaml_schema_value_t config_schema = {
+    CYAML_VALUE_MAPPING(CYAML_FLAG_POINTER, struct config, top_mapping_schema),
+};
+
+static const cyaml_config_t config = {
+    .log_fn = cyaml_log,            /* Use the default logging function. */
+    .mem_fn = cyaml_mem,            /* Use the default memory allocator. */
+    .log_level = CYAML_LOG_WARNING, /* Logging errors and warnings only. */
+};
+
 int main(int argc, const char **argv) {
+    struct config *conf;
+
+    // Load YAML
+    cyaml_err_t err;
+    // err = cyaml_load_data((const uint8_t *)yaml, strlen(yaml), &config, &config_schema,
+    //                       (void *)&conf, NULL);
+    printf("%s\n", argv[1]);
+    err = cyaml_load_file(argv[1], &config, &config_schema, (void **)&conf, NULL);
+
+    if (err != CYAML_OK) {
+        printf("Error loading YAML: %s\n", cyaml_strerror(err));
+        return 1;
+    }
+
+    // Load BPF program
+    struct l4_lb_bpf *skel = NULL;
+
+    log_info("Opening BPF skel");
+    skel = l4_lb_bpf__open();
+    if (!skel) {
+        log_fatal("Error while opening BPF skeleton");
+        exit(1);
+    }
+
+    bpf_program__set_type(skel->progs.l4_lb, BPF_PROG_TYPE_XDP);
+    int backend_map = bpf_map__fd(skel->maps.backend_map);
+    if (backend_map < 0) {
+        log_error("Failed to get file descriptor of BPF map: %s", strerror(errno));
+        return 1;
+    }
+
+    // Access data
+    printf("VIP: %s\n", conf->vip);
+    printf("Backends %ld:\n", conf->backends_count);
+    size_t next_idx = 0;
+    for (size_t i = 0; i < conf->backends_count; i++) {
+        log_info("Loading IP %s", conf->backends[i].ip);
+
+        // Convert the IP to an integer
+        struct in_addr addr;
+        int ret = inet_pton(AF_INET, conf->backends[i].ip, &addr);
+        if (ret != 1) {
+            log_error("Failed to convert IP %s to integer", conf->backends[i].ip);
+            continue;
+        }
+
+        bpf_map_update_elem(backend_map, &next_idx, &addr, 0);
+        next_idx += 1;
+    }
+
+    // Free memory
+    cyaml_free(&config, &config_schema, conf, 0);
     return 0;
 }
